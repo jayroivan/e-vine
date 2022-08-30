8e0a5737849a509e147754b362c3adf390e5378a
jayroivan <ma0721032017@unab.edu.sv>
2022-08-30 15:12:28 -0600

    second commit

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, ObjectId } from 'mongoose';
 
 export type UserDocument = Usuario & Document;
 
@@ -19,13 +19,9 @@ export class Usuario {
   @Prop()
   telefono: number;
 
-<<<<<<< HEAD
+
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Rol', required: true })
-  rol: string
-=======
-  @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Rol', required: true})
-  rol: string;
->>>>>>> c5826111346ec11b85c56f6406d417992d9f8cfd
+  rol: ObjectId
 }
 
 export const UsuarioSchema = SchemaFactory.createForClass(Usuario);
